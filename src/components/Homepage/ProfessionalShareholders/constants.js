c948783bafb7a97572147543ee1233d0e6983c30
kirankumar-medikurthy <kirankumarmedikurthy@gmail.com>
2025-01-04 15:03:36 +0530

    feat: updated the profile photos

--- src/components/Homepage/ProfessionalShareholders/constants.js
+++ src/components/Homepage/ProfessionalShareholders/constants.js
@@ -12,23 +12,20 @@ export const SHARE_HOLDERS = [
     name: "S SHYAM SUNDAR",
     role: "Co-Founder & CTO",
     linkedin: "https://www.linkedin.com/in/s-shyam-sundar-51a0b0163/",
-    profilePhoto:
-      "https://media.licdn.com/dms/image/v2/D5603AQHbnKoDEV7haw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1720250413204?e=1741219200&v=beta&t=rjZYZjftT7xOR0FaCCmVDb5jvhyFzNm717tjeMjkA9g",
+    profilePhoto: "/images/shyam-profile-photo.jpeg",
   },
   {
     id: 3,
     name: "B BHEERU GOWDU",
     role: "Chief Operating Officer",
-    linkedin: "https://www.linkedin.com/in/s-shyam-sundar-51a0b0163/",
-    profilePhoto:
-      "https://media.licdn.com/dms/image/v2/D5603AQHbnKoDEV7haw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1720250413204?e=1741219200&v=beta&t=rjZYZjftT7xOR0FaCCmVDb5jvhyFzNm717tjeMjkA9g",
+    linkedin: "https://www.linkedin.com/in/bheerugowdu-bangennagari/",
+    profilePhoto: "/images/gowda-profile-photo.jpg",
   },
   {
     id: 4,
-    name: "KIRAN KUMAR MEDIKURTHY",
+    name: "M KIRAN KUMAR",
     role: "Vice President",
     linkedin: "https://www.linkedin.com/in/kirankumar-medikurthy/",
-    profilePhoto:
-      "https://media.licdn.com/dms/image/v2/D5635AQFVuxgr4iyVfA/profile-framedphoto-shrink_800_800/profile-framedphoto-shrink_800_800/0/1734877702189?e=1736143200&v=beta&t=5QpJfrImX54Km8GfAhd9Yav_CQGosVq98FEFZhNfhyc",
+    profilePhoto: "/images/kirankumar-photo-id.jpg",
   },
 ];
